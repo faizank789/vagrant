99cfb63ea86873025ca862751f076344fd2b7f01
Faizan <faizank789@gmail.com>
2022-08-28 02:02:31 +0530

    updated vagrant && worker.sh

--- kubernetes_setup/kube/worker.sh
+++ kubernetes_setup/kube/worker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-set -e
 
-master_ip=`dig master +short` && \
+master_ip=`grep "master" /etc/hosts | awk '{print $1}'` && \
 
-kubeadm join $master_ip:6443 --token yi6muo.4ytkfl3l6vl8zfpk \
---discovery-token-unsafe-skip-ca-verification
+kubeadm join $master_ip:6443 --token yi6muo.4ytkfl3l6vl8zfpk --discovery-token-unsafe-skip-ca-verification
 
