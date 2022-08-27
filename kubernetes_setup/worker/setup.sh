4954fa1ccb889a9ea168085747f2584b4e9ffa21
Faizan <faizank789@gmail.com>
2022-08-27 20:15:18 +0530

    updated code

--- kubernetes_setup/worker/setup.sh
+++ kubernetes_setup/worker/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip=sudo hostname -I && sudo echo "worker $ip" >> /etc/hosts  
 sudo bash ../basic/config.sh
 sudo bash ../docker/setup.sh
 sudo bash ../kube/setup.sh
+sudo bash ../kube/access_token.sh
