2750d574729f73a5cedfda12639526d22c257c11
Faizan <faizank789@gmail.com>
2022-08-28 00:48:10 +0530

    fixed code issue

--- kubernetes_setup/kube/master.sh
+++ kubernetes_setup/kube/master.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
+sudo kubeadm config images pull && \
+sudo kubeadm init --token yi6muo.4ytkfl3l6vl8zfpk --apiserver-advertise-address=master --ignore-preflight-errors=all
 sudo mkdir -p $HOME/.kube && \
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && \
 sudo chown $(id -u):$(id -g) $HOME/.kube/config && \
-sudo kubeadm config images pull && \
-sudo kubeadm init --token yi6muo.4ytkfl3l6vl8zfpk --apiserver-advertise-address=master --ignore-preflight-errors=all
 sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 
