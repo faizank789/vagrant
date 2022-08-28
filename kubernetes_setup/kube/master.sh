c0acd640b19ab30fa10dd7cf66be17c652842998
Faizan <faizank789@gmail.com>
2022-08-29 00:17:01 +0530

    updated master.sh

--- kubernetes_setup/kube/master.sh
+++ kubernetes_setup/kube/master.sh
@@ -6,7 +6,8 @@ sudo kubeadm init --token yi6muo.4ytkfl3l6vl8zfpk --apiserver-advertise-address=
 sudo mkdir -p $HOME/.kube && \
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && \
 sudo chown $(id -u):$(id -g) $HOME/.kube/config && \
-sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" && \
+sudo kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
 
 
 
