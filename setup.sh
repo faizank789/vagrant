4deb34ed580d09b06ca797c11723bb741a68877b
Faizan <faizank789@gmail.com>
2022-08-28 23:50:59 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg && \
 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list && \
+ 
+sudo apt-get install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev && \
 
 sudo apt update && sudo apt install vagrant && \ 
 
