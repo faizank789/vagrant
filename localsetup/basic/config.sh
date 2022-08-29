da7c37e9a034c48d56eed3e6b6fc36052b5290af
Faizan <faizank789@gmail.com>
2022-08-29 11:47:01 +0530

    updated config.sh

--- localsetup/basic/config.sh
+++ localsetup/basic/config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo -i && \
+sudo echo "root" | passwd --stdin root && \
 yum update -y && \
 yum install wget git -y && \
 sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config && \
 sudo systemctl stop firewalld.service && \
 sudo systemctl disable firewalld.service && \
-sudo sed -E -i 's|^#?(PasswordAuthentication)\s.*|\1 yes|' /etc/ssh/sshd_config && sudo sshd -t && sudo systemctl restart sshd.service && \
-init 6
+sudo sed -E -i 's|^#?(PasswordAuthentication)\s.*|\1 yes|' /etc/ssh/sshd_config && sudo sshd -t && sudo systemctl restart sshd.service
+init 6  
 
