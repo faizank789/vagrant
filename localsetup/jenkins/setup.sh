2998a4e83d94215abb977daee4d00de7224d428f
Faizan <faizank789@gmail.com>
2022-08-30 15:31:30 +0530

    updated code

--- localsetup/jenkins/setup.sh
+++ localsetup/jenkins/setup.sh
@@ -5,7 +5,8 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo && \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key && \
 
-sudo yum upgrade && \
-sudo yum install java-11-openjdk && \
-sudo yum install jenkins && \
+sudo yum upgrade -y && \
+sudo yum install java-11-openjdk -y && \
+sudo yum install jenkins -y && \
+sudo systemctl enable jenkins && sudo systemctl start jenkins && \
 sudo systemctl daemon-reload && \
